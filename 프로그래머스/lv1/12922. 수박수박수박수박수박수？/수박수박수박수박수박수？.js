6a77156fd6af364f93534407774e4b96d3fa9126
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-12-22 00:36:45 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let answer = '';
+    
+    
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 !== 0) answer += '수'
+        if (i % 2 === 0) answer += '박'
+    }
+    return answer;
+}
