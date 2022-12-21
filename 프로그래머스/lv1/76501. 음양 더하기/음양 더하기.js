5cc9be9031dee32f1147dc7009c3d3e1af914514
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-12-21 16:46:47 +0900

    [level 1] Title: 음양 더하기, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,9 @@
+function solution(absolutes, signs) {
+    let answer = 0;
+    
+    for(let i = 0; i < absolutes.length; i++) {
+        if(signs[i] === true) answer += absolutes[i];
+        if(signs[i] === false) answer -= absolutes[i];
+    }
+    return answer;
+}
