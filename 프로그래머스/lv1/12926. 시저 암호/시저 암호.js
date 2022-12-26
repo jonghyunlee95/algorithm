2a6d2b3ceb945985e6c9b4f77160d08ae9d58723
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-12-26 20:41:06 +0900

    [level 1] Title: 시저 암호, Time: 3.40 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,21 @@
+function solution(s, n) {
+    let answer = '';
+    let arr = s.split('');
+    
+    for (let i = 0; i < arr.length; i++) {
+        let ascii = arr[i].charCodeAt();
+        
+        if (ascii >= 65 && ascii <= 90) {
+            ascii += n;
+            if(ascii > 90) ascii -= 26;
+        }
+        if (ascii >= 97 && ascii <=122) {
+            ascii += n;
+            if (ascii > 122) ascii -= 26;
+        }
+        
+        answer += String.fromCharCode(ascii);
+    }
+    
+    return answer;
+}
