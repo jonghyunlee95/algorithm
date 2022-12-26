320e9dd30f7185adb5a95a84df04000bed511808
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-12-27 02:38:12 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,27 @@
+function solution(n, arr1, arr2) {
+    const answer = [];
+    const arr = [];
+    let word = '';
+    
+    for (let i = 0; i < arr1.length; i++) {
+        arr1[i] = arr1[i].toString(2);
+        arr2[i] = arr2[i].toString(2);
+        
+        if(arr1[i].length !== 0) 
+        
+        answer.push(Number(arr1[i]) + Number(arr2[i]));
+        
+        let str = String(answer[i]);
+        if (str.length !== n) word += ' '.repeat(n - str.length);
+        
+        for (let j = 0; j < str.length; j++) {
+            if (str[j] > 0) word += '#';
+            if (str[j] == 0) word += ' ';
+            
+        }
+        
+        arr.push(word);
+        word = '';
+    }
+    return arr;
+}
