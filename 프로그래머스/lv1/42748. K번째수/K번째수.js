101e2b193b735095f5cff01e15d5671835872faf
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-12-28 03:16:06 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,19 @@
+function solution(array, commands) {
+    const answer = [];
+    
+    for (let i = 0; i < commands.length; i++) {
+        let arr = [];
+        let first = commands[i][0] - 1;
+        let last = commands[i][1] - 1;
+        let idx = commands[i][2] - 1;
+        
+        for (let j = first; j <= last; j++) {
+            arr.push(array[j])
+        }
+        
+        arr.sort((a, b) => a - b);
+        answer.push(arr[idx])
+    }
+    
+    return answer;
+}
