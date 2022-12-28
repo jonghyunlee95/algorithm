bc1fc3252708e30b6e71d983a78ad93398911b23
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-12-29 04:10:48 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+  
+    const week = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    const days = [31,29,31,30,31,30,31,31,30,31,30,31]; 
+    let day = b + 4;
+    
+    for(let i = 0; i < a-1; ++i){
+        day += days[i];
+    }
+    return week[day % 7];
+}
