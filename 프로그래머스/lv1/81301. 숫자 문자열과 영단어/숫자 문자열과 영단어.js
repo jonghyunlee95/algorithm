c86484a8a810acf053c66944c019bb42f691e9d9
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-12-29 00:03:04 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const str_num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+   
+    
+    for (let i = 0; i < str_num.length; i++) {
+        while (s.includes(str_num[i])) {
+            s = s.replace(str_num[i], num[i]);
+        }
+    }
+    
+    return Number(s);
+}
