e1fe0c2222b6e65361365928c903a510faccf8ac
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-12-29 19:12:24 +0900

    [level 1] Title: 폰켓몬, Time: 0.73 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,12 @@
+function solution(nums) {
+    let answer = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (!answer.includes(nums[i])) answer.push(nums[i]);
+        
+    }
+    if (answer.length > nums.length / 2) return nums.length / 2;
+    
+    
+    return answer.length;
+}
