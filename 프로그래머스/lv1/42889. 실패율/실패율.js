893b6fd716065de6edbdaf2904b846c559aa4cc2
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-12-31 19:28:22 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,19 +1,13 @@
 function solution(N, stages) {
-    const answer = [];
-    const arr = []; 
+    const answer = []; 
     let man = stages.length;
     
     for (let i = 1; i <= N; i++) {
         let fail = stages.filter(a => a === i).length / man;
         man -= stages.filter(a => a === i).length;
-        arr.push([i, fail]);
-        arr.sort((a, b) => b[1] - a[1]);
+        answer.push([i, fail]);
+        answer.sort((a, b) => b[1] - a[1]);
     }
     
-    for (let i = 0; i < arr.length; i++) {
-        answer.push(arr[i][0]);
-    }
-    
-    
-    return answer;
+    return answer.map(x => x[0]);
 }
