0f31f1b7057c179c5bda1ff405daa4faa7c60ff4
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-01-03 20:18:42 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,20 +1,20 @@
 function solution(k, score) {
-    const honor = [];
-    const result = [];
+    const arr = [];
+    const answer = [];
     
-    for(let i = 0; i < score.length; i++) {
-        if(i < k) {
-            honor.push(score[i])
+    for (let i = 0; i < score.length; i++) {
+        if (i < k) {
+            arr.push(score[i]);
         }
         
-        if (score[i] > Math.min(...honor)) {
-            honor.pop();
-            honor.push(score[i]);
-            honor.sort((a,b) => b - a);
+        if (score[i] > Math.min(...arr)) {
+            arr.pop();
+            arr.push(score[i]);
+            arr.sort((a, b) => b - a);
         }
         
-        result.push(honor.at(-1));
+        answer.push(arr[arr.length - 1]);
     }
     
-    return result;
+    return answer;
 }
