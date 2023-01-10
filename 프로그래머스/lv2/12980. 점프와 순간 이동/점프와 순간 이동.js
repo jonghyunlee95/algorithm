fee97b604c2907b9a94819e27058bd7aafba2a06
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-01-10 20:27:22 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n){
+    let ans = 0;
+    
+    while (n !== 0) { 
+        if (n % 2 !== 0) {
+            n = Math.floor(n / 2);
+            ans++;
+        } else {
+            n = n / 2;
+        }
+    }
+
+    return ans;
+}
