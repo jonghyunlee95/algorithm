b34e07e1dcd8413648620ce0be69eee9f606006d
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-02-03 03:08:26 +0900

    [Silver V] Title: 영화감독 숌, Time: 444 ms, Memory: 23248 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = +require('fs').readFileSync(filePath).toString();
+let cnt = 1;
+let num = 666;
+
+while (cnt !== input) {
+  num++;
+  if (String(num).includes('666')) cnt++;
+}
+
+console.log(num);
