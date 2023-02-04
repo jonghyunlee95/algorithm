bbb4f51d19c0d053260b981a4d893c012c577f27
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-02-04 21:15:20 +0900

    [Silver III] Title: 문자열 집합, Time: 176 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.js
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.js
@@ -2,32 +2,12 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const [T, ...input] = require('fs').readFileSync(filePath).toString().split('\n');
 const num1 = +T.split(' ')[0];
 const num2 = +T.split(' ')[1];
-const arr1 = input
-  .splice(0, num1)
-  .sort()
-  .map((x) => x.trim());
-const arr2 = input.sort().map((x) => x.trim());
+const arr1 = new Set(input.splice(0, num1).map((s) => s.trim()));
+const arr2 = input.map((x) => x.trim());
 let cnt = 0;
 
-function binary(arr, el) {
-  let start = 0;
-  let end = arr.length - 1;
-  let middle = Math.floor((start + end) / 2);
-
-  while (arr[middle] !== el && start <= end) {
-    if (el < arr[middle]) end = middle - 1;
-    else start = middle + 1;
-    middle = Math.floor((start + end) / 2);
-  }
-
-  if (arr[middle] === el) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 for (let i = 0; i < arr2.length; i++) {
-  if (binary(arr1, arr2[i])) cnt++;
+  if (arr1.has(arr2[i])) cnt++;
 }
+
 console.log(cnt);
