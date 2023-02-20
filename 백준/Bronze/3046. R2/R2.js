0c86e28bdc52fd19ffffa6cee2af2b2a263eb1d0
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-02-21 03:02:08 +0900

    [Bronze IV] Title: R2, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.js
@@ -0,0 +1,9 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(' ')
+  .map((x) => +x);
+
+console.log(input[1] * 2 - input[0]);
