f655a07feb78102a9ccb30b5e69b7849d838bb3d
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-03-17 00:34:18 +0900

    [Bronze I] Title: 유레카 이론, Time: 2308 ms, Memory: 9872 KB -BaekjoonHub

---
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.js
@@ -0,0 +1,30 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((x) => +x);
+const T = input.shift();
+const arr = [];
+const answer = [];
+
+for (let i = 1; i <= 1000; i++) {
+  for (let j = 1; j <= 1000; j++) {
+    for (let k = 1; k <= 1000; k++) {
+      const num1 = (i * (i + 1)) / 2;
+      const num2 = (j * (j + 1)) / 2;
+      const num3 = (k * (k + 1)) / 2;
+      if (num1 + num2 + num3 <= 1000) {
+        if (!arr.includes(num1 + num2 + num3)) arr.push(num1 + num2 + num3);
+      }
+    }
+  }
+}
+
+for (let i = 0; i < T; i++) {
+  if (arr.includes(input[i])) answer.push(1);
+  else answer.push(0);
+}
+
+console.log(answer.join('\n'));
