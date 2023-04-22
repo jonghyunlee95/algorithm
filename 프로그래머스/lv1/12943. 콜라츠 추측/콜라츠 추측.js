3aca82c17f1b0800a6f688046ecd35a6dc329cb4
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-04-22 19:42:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,21 +1,16 @@
 function solution(num) {
     let answer = 0;
     
-    if(num === 1) return answer;
-    
-    while(num !== 1) {
-        if(num % 2 === 0 && num > 1) {
+    while(num !== 1 && answer !== 500) {
+        if(num % 2 === 0) {
             num = num / 2;
-            answer += 1;
-        }
-        if(num % 2 !== 0 && num > 1) {
+            answer ++;
+        } else {
             num = (num * 3) + 1;
-            answer += 1;
-        }
-        if(answer > 500) {
-            answer = -1;
-            return answer;
+            answer ++;
         }
     }
-    return answer;
+    
+    if (num === 1) return answer;
+    else return -1;
 }
