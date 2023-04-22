4600606959038be6fda85072710cefb6ecace1bc
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-04-22 15:29:46 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,9 +1,5 @@
 function solution(n) {
-    let answer = 0;
-    let arr = String(n).split('').map((x) => +x);
-    
-    for (let i = 0; i < arr.length; i++) {
-        answer += arr[i];
-    }
-    return answer;
+    const arr = String(n).split('').map((x) => +x);
+
+    return arr.reduce((a, b) => a + b);
 }
