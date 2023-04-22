db37dba73c0c0d299a371c4493ba426c682db864
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-04-22 17:31:13 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,8 +1,6 @@
 function solution(n) {
-    let answer = Math.sqrt(n);
-    
-    if (answer % 1 === 0) answer = Math.pow(answer + 1, 2);
-    if (answer % 1 !== 0) answer = -1; 
-    
-    return answer;
+    const answer = Math.sqrt(n);
+  
+    if (answer % 1 === 0) return Math.pow(answer + 1, 2);
+    if (answer % 1 !== 0) return -1; 
 }
