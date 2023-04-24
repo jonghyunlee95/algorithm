7b54d4040bc48f99f3ae1cc7ed11cd6fcc562bab
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-04-24 20:21:56 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.46 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,28 @@
+function solution(survey, choices) {
+    let answer = '';
+    const arr = ['R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'];
+    const num = [0, 0, 0, 0, 0, 0, 0, 0];
+    
+    for (let i = 0; i < survey.length; i++) {
+        const el = survey[i].split('');
+        
+        if (choices[i] < 4) {
+            if (choices[i] === 3) num[arr.indexOf(el[0])] += 1;                             if (choices[i] === 2) num[arr.indexOf(el[0])] += 2;
+            if (choices[i] === 1) num[arr.indexOf(el[0])] += 3;
+        } else if (choices[i] > 4) {
+            if (choices[i] === 5) num[arr.indexOf(el[1])] += 1;
+            if (choices[i] === 6) num[arr.indexOf(el[1])] += 2;
+            if (choices[i] === 7) num[arr.indexOf(el[1])] += 3;
+        } 
+    }
+    
+    for (let i = 0; i < 4; i++) {
+        const name = arr.splice(0, 2);
+        const score = num.splice(0, 2);
+
+        if (score[0] > score[1]) answer += name[0];
+        if (score[0] < score[1]) answer += name[1];
+        if (score[0] === score[1]) answer += name.sort()[0];
+    }
+    return answer;
+}
