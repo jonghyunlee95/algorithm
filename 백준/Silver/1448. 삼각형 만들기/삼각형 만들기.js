b0693dd3cc08807a4d5e7a4528c4acd2150513d3
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-05-09 22:31:23 +0900

    [Silver III] Title: 삼각형 만들기, Time: 972 ms, Memory: 102084 KB -BaekjoonHub

---
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+let input = (fs.readFileSync('./dev/stdin') + '').toString().trim().split('\n');
+const n = +input.shift();
+
+input = input.map((a) => parseInt(a)).sort((a, b) => b - a);
+
+let ans = 0;
+
+const checkTri = (a, b, c) => {
+  return a < b + c;
+};
+
+for (let i = 0; i < n - 2; i++) {
+  if (checkTri(input[i], input[i + 1], input[i + 2])) {
+    ans = [input[i], input[i + 1], input[i + 2]].reduce(
+      (acc, cur) => acc + cur
+    );
+    break;
+  }
+}
+
+console.log(ans === 0 ? -1 : ans);
