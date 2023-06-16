0bc9cfe91ca82c0abcb18fe37b70e51e047f5c56
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-06-17 03:07:04 +0900

    [level 2] Title: 짝지어 제거하기, Time: 48.27 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,12 +1,19 @@
 function solution(s){
+    // 스택에 이용할 빈 배열 선언
     const stack = [];
     
+    // 문자열 s를 반복문을 이용해 확인합니다.
     for (let i = 0; i < s.length; i++) {
-        if (stack[stack.length - 1] !== s[i]) stack.push(s[i]);
-        else stack.pop();
+      // 배열의 마지막 값과 현재 s[i]의 값을 비교하고 같지 않다면 
+      // stack에 s[i]값을 push해줍니다.
+      // 같다면 stack의 마지막값을 pop해줍니다.
+		  if (stack[stack.length - 1] !== s[i]) stack.push(s[i]);
+      else stack.pop();
     }
     
+    // 모든 과정을 끝낸 후 stack배열의 길이를 확인하고
+    // 0이라면 1을 0이 아니라면 0을 return 해줍니다.
     // if (!stack.length) return 1;
     // else return 0;
-    return !stack.length ? 1 : 0 
+    return !stack.length ? 1 : 0
 }
