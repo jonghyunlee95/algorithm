9fdaba0917e696682578870c9f191dd68413a8d0
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-06-16 16:15:16 +0900

    [level 2] Title: 짝지어 제거하기, Time: 35.17 ms, Memory: 47 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,18 +1,21 @@
 function solution(s){
-    let answer = '';
-    let arr = [];
+    // 스택에 이용할 빈 배열 선언
+    const arr = [];
     
+    // 문자열 s를 반복문으로 확인하며
     for (let i = 0; i < s.length; i++) {
+        // 배열의 마지막 값과 현재 s[i]의 값을 비교하고 같지 않다면 
         if (arr[arr.length - 1] !== s[i]) {
+            // arr에 s[i]값을 push해줍니다.
             arr.push(s[i]);
         } else {
+            // 같다면 arr의 마지막값을 pop해줍니다.
             arr.pop();
         }
     }
     
-    if (arr.length === 0) {
-        return 1;
-    } else {
-        return 0;
-    }
+    // 모든 과정을 끝낸 후 stack에 사용한 배열의 길이를 확인하고
+    // 0이라면 1을 0이 아니라면 0을 return 해줍니다.
+    if (!arr.length) return 1;
+    else return 0;
 }
