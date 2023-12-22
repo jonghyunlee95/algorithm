175868586775b405e99f9713ce30f4053853cec1
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2023-12-22 20:05:19 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,17 +1,5 @@
 function solution(nums) {
-    const pocketmonMap = new Map();
-    let answer = 0;
+    const pocketmon = [...new Set(nums)];
     
-    for (let i = 0; i < nums.length; i++) {
-        const pocketmon = nums[i];
-        
-        if (!pocketmonMap.get(pocketmon)) pocketmonMap.set(pocketmon, 1);
-        else pocketmonMap.set(pocketmon, pocketmonMap.get(pocketmon) + 1);
-    }
-
-    pocketmonMap.forEach((value, key, idx) => {
-        answer++;
-    })
-
-    return answer < nums.length / 2 ? answer : nums.length / 2
+    return pocketmon.length < nums.length / 2 ? pocketmon.length: nums.length / 2
 }
