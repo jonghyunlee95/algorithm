884f1c2892ed8e631160b1574ef54a373998d826
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2023-12-24 21:03:54 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,12 +1,11 @@
 function solution(array, commands) {
     const answer = [];
     
-    for (let i = 0; i < commands.length; i++) {
-        const start = commands[i][0] - 1;
-        const end = commands[i][1];
+    commands.map((item) => {
+        const [start, end, findIdx] = item;
         
-        answer.push(array.slice(start, end).sort((a, b) => a - b)[commands[i][2] - 1]);
-    }
+        answer.push(array.slice(start - 1, end).sort((a, b) => a - b)[findIdx - 1]);
+    })
    
     return answer;
 }
