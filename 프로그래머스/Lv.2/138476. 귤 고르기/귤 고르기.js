f7581ee727ce8690d806a1babd2bdd9bae805433
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2024-01-15 14:50:36 +0900

    [level Lv.2] Title: 귤 고르기, Time: 35.76 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/Lv.2/138476. 귤 고르기/귤 고르기.js
@@ -1,6 +1,6 @@
 function solution(k, tangerine) {
     const map = new Map();
-    let answer = 1;
+    let answer = 0;
     
     for (let i = 0; i < tangerine.length; i++) {
         if (!map.get(tangerine[i])) map.set(tangerine[i], 1);
@@ -10,10 +10,10 @@ function solution(k, tangerine) {
     const sortedMap = [...map].sort((a, b) => b[1] - a[1]);
     
     for (let i = 0; i < sortedMap.length; i++) {
+        answer++;
+        
         if (k > sortedMap[i][1]) k -= sortedMap[i][1];
         else break;
-        
-        answer++;
     }
     
     return(answer)
