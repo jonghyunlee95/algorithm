da7e57066ed31a319da1d48a6fb520582c895aef
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2024-01-29 14:31:22 +0900

    [Silver II] Title: 좌표 압축, Time: 1624 ms, Memory: 298168 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -1,17 +1,20 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
-const T = +input.shift();
-const arr = input[0].split(' ').map(Number);
-const uniqueArr = [...new Set([...arr].sort((a, b) => a - b))];
+const [N, input] = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+const arr = input.split(' ').map(Number);
+const sortedArr = [...new Set(arr)].sort((a, b) => a - b);
 const map = new Map();
-const answer = [];
+let answer = '';
 
-for (let i = 0; i < uniqueArr.length; i++) {
-  map.set(uniqueArr[i], i);
+for (let i = 0; i < arr.length; i++) {
+  map.set(sortedArr[i], i);
 }
 
 for (let i = 0; i < arr.length; i++) {
-  answer.push(map.get(arr[i]));
+  answer += map.get(arr[i]) + ' ';
 }
 
-console.log(answer.join(' '));
+console.log(answer);
