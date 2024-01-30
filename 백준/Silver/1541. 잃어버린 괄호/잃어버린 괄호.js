a0c674f6808c897502f7392bc2ac1d04ecd82d8f
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2024-01-30 14:54:34 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,20 +1,23 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = require('fs').readFileSync(filePath).toString().trim().split('-');
-input = input.map((x) => x.split('+'));
-
-const arr = [];
+const input = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('-')
+  .map((item) => item.split('+'));
 
 for (let i = 0; i < input.length; i++) {
-  let sum = 0;
-  for (let j = 0; j < input[i].length; j++) {
-    sum += +input[i][j];
+  if (input[i].length > 1) {
+    input[i] = input[i].map(Number).reduce((a, b) => a + b);
+  } else {
+    input[i] = input[i].map(Number)[0];
   }
-  arr.push(sum);
 }
 
-let answer = arr[0];
+let cnt = input[0];
 
-for (let i = 1; i < arr.length; i++) {
-  answer -= arr[i];
+for (let i = 1; i < input.length; i++) {
+  cnt -= input[i];
 }
-console.log(answer);
+
+console.log(cnt);
