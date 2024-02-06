81de5b468236345ae116000f7424a9c3afa8fc21
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2024-02-06 15:03:02 +0900

    [Silver IV] Title: 박 터뜨리기, Time: 140 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.js
+++ 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.js
@@ -1,15 +1,20 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let [n, k] = require('fs').readFileSync(filePath).toString().trim().split(' ').map(Number);
+let [N, K] = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
 let sum = 0;
 
-for (let i = 1; i <= k; i++) {
+for (let i = 1; i < K + 1; i++) {
   sum += i;
 }
 
-if (sum > n) {
+if (sum > N) {
   console.log(-1);
 } else {
-  n -= sum;
-  if (n % k === 0) console.log(k - 1);
-  else console.log(k);
+  N -= sum;
+  if (N % K === 0) console.log(K - 1);
+  else console.log(K);
 }
