fa1e5af1d8e2abdf10db6ca2a3227fb0c9b434fb
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2024-02-19 14:53:44 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 824 ms, Memory: 161732 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -2,26 +2,22 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = require('fs')
   .readFileSync(filePath)
   .toString()
-  .split('\n')
-  .map((x) => x.trim());
-const T1 = +input.shift();
-const have = input.shift().split(' ');
-const T2 = +input.shift();
-const find = input[0].split(' ');
-const obj = {};
+  .trim()
+  .split('\n');
+const N = +input[0];
+const numCard = input[1].split(' ').map(Number);
+const M = +input[2];
+const sCard = input[3].split(' ').map(Number);
 const map = new Map();
 const answer = [];
 
-for (let i = 0; i < T1; i++) {
-  obj[have[i]] = (obj[have[i]] || 0) + 1;
+for (let i = 0; i < N; i++) {
+  if (!map.has(numCard[i])) map.set(numCard[i], 1);
+  else map.set(numCard[i], map.get(numCard[i]) + 1);
 }
 
-for (let i = 0; i < T1; i++) {
-  map.set(have[i], obj[have[i]]);
-}
-
-for (let i = 0; i < T2; i++) {
-  if (map.has(find[i])) answer.push(map.get(find[i]));
+for (let j = 0; j < M; j++) {
+  if (map.get(sCard[j])) answer.push(map.get(sCard[j]));
   else answer.push(0);
 }
 
