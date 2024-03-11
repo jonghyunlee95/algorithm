ac3d43bbea6d1115807b91fb7119b8d2c62c331f
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2024-03-11 13:32:00 +0900

    [Silver III] Title: N과 M (8), Time: 188 ms, Memory: 12736 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.js
@@ -0,0 +1,29 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+const [N, M] = input[0].split(' ').map(Number);
+const arr = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+const answer = [];
+
+const dfs = (selected, index) => {
+  if (selected.length === M) {
+    answer.push(selected.join(' '));
+    return;
+  }
+
+  for (let i = index; i < N; i++) {
+    selected.push(arr[i]);
+    dfs(selected, i);
+    selected.pop();
+  }
+};
+
+dfs([], 0);
+
+console.log(answer.join('\n').trim());
